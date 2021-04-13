3730a47f4c7d017addfd933641d1c32d745ec002
Patrik <lrpatrk55@gmail.com>
2021-04-13 11:45:57 +0200

    Footer bug fix

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 
   $('.menu-icon').click(function(){
     $('.nav-list').fadeToggle(100);
+    $('.menu-icon i').toggleClass('fa-bars');
+    $('.menu-icon i').toggleClass('fa-times');
   })
 
 });
