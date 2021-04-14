1ffa68a4b91cea52f459395ed4a36bdbfe3e475b
Patrik <lrpatrik55@gmail.com>
2021-04-14 13:11:44 +0200

    Cart system #2

--- js/cart.js
+++ js/cart.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
 
-  if ($('.cart-content').length) {
-    var imageSrc = localStorage.getItem("imageSrc", imageSrc);
-    var productName = localStorage.getItem("productName", productName);
-    var productPrice = localStorage.getItem("productPrice", productPrice);
-    addItemToCart(imageSrc, productName, productPrice)
-  }
-
   //Remove cart item
-  $('.cart-item-remove').click(function(){
+  $('.cart-content').on("click", ".cart-item-remove", function(){
+    var i = $(this).parent().attr('id');
+
     $(this).parent().remove();
+
+    localStorage.removeItem('imageSrc' + i);
+    localStorage.removeItem('productName' + i);
+    localStorage.removeItem('productPrice' + i);
+
     updateCartTotal();
   })
 
   //Updates the cart total when quantity changed
-  $('.cart-item-quantity').change(function(){
+  $('.cart-content').on("change", ".cart-item-quantity", function(){
     var quantity = $(this);
     var quantityValue = $(this).val();
     if (quantityValue < 1 || quantityValue > 200) {
@@ -23,21 +23,65 @@ $(document).ready(function(){
     updateCartTotal();
   })
 
+  //Download cart items
+  if ($('.cart-content').length) {
+
+    var i = 1;
+
+    while (true) {
+      var productNameItem = 'productName' + i;
+      if (localStorage.getItem(productNameItem)) {
+        var imageSrc = localStorage.getItem('imageSrc' + i);
+        var productName = localStorage.getItem('productName' + i);
+        var productPrice = localStorage.getItem('productPrice' + i);
+        console.log(productName);
+        addItemToCart(imageSrc, productName, productPrice, i)
+      } else {
+        return true;
+      }
+      i++;
+    }
+  }
+
   //Save product's informations when "add to cart button" is clicked
   $('button[name=addtocart]').click(function(){
     var imageSrc = $(this).siblings('img').attr('src');
-    localStorage.setItem("imageSrc", imageSrc);
     var productName = $(this).siblings('.product-name').text();
-    localStorage.setItem("productName", productName);
     var productPrice =  $(this).siblings('.product-price').text();
-    localStorage.setItem("productPrice", productPrice);
+
+    var i = 1;
+
+    while (true) {
+
+      var imageSrcItem = "imageSrc" + i;
+      var productNameItem = "productName" + i;
+      var productPriceItem = "productPrice" + i;
+
+      if (!localStorage.getItem(productNameItem)) {
+        localStorage.setItem(imageSrcItem, imageSrc);
+        localStorage.setItem(productNameItem, productName);
+        localStorage.setItem(productPriceItem, productPrice);
+        return true;
+      }
+      i++;
+    }
   })
 
-  function addItemToCart(imageSrc, productName, productPrice) {
+  //Add item to cart
+  function addItemToCart(imageSrc, productName, productPrice, i) {
     var cartRow = document.createElement('div');
-    $(cartRow).text(productName)
+    $(cartRow).addClass('cart-row');
+    $(cartRow).attr('id', i);
     var cartItems = $('.cart-content');
+    var cartRowContents = `
+      <img src="${imageSrc}" alt="">
+      <p class="cart-item-name">${productName}</p>
+      <input type="number" class="cart-item-quantity" value="1">
+      <button type="button" class="cart-item-remove"><i class="far fa-trash-alt"></i></button>
+      <p class="cart-item-price">${productPrice}</p>`
+    cartRow.innerHTML = cartRowContents;
     cartItems.append(cartRow);
+    updateCartTotal();
   }
 
   //Updates the cart total
@@ -53,5 +97,6 @@ $(document).ready(function(){
     total = Math.round(total * 100) / 100;
     $('.cart-total').text('€' + total);
   }
+
 
 })
