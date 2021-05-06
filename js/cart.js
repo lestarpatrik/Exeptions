5b88ee88a35f7d32797d6aa207e849a82bfd1ebf
Patrik <lrpatrik55@gmail.com>
2021-05-06 18:22:12 +0200

    Delete purchase system

--- js/cart.js
+++ js/cart.js
@@ -9,6 +9,14 @@ $(document).ready(function(){
     }
   }
 
+  //Remove every item from cart when clicking purchase
+  $('.purchase-button').click(function(){
+    $('.cart-content').children().remove();
+    localStorage.clear();
+    emptyCart();
+    updateCartTotal();
+  })
+
   //Remove cart item
   $('.cart-content').on("click", ".cart-item-remove", function(){
     var i = $(this).parent().attr('id');
@@ -45,8 +53,7 @@ $(document).ready(function(){
         var imageSrc = localStorage.getItem('imageSrc' + i);
         var productName = localStorage.getItem('productName' + i);
         var productPrice = localStorage.getItem('productPrice' + i);
-        var id = localStorage.getItem('id');
-        addItemToCart(imageSrc, productName, productPrice, i, id)
+        addItemToCart(imageSrc, productName, productPrice, i)
       } else {
         return true;
       }
@@ -57,10 +64,8 @@ $(document).ready(function(){
   //Save product's informations when "add to cart button" is clicked
   $('button[name=addtocart]').click(function(){
     $('.product-added-to-cart-bg').show();
-    var shopItem = $(this).parent();
     var productName = $(this).siblings('.product-name').text();
     var productPrice =  $(this).siblings('.product-price').text();
-    var id = $(shopItem).attr('data-item-id');
 
     if ($('.product-info').length) {
       var imageSrc = $(this).parent().siblings('img').attr('src').replace('../', '');
@@ -80,8 +85,6 @@ $(document).ready(function(){
         localStorage.setItem(imageSrcItem, imageSrc);
         localStorage.setItem(productNameItem, productName);
         localStorage.setItem(productPriceItem, productPrice);
-        localStorage.setItem('id', id);
-
         return true;
       }
       i++;
@@ -94,10 +97,9 @@ $(document).ready(function(){
   })
 
   //Add item to cart
-  function addItemToCart(imageSrc, productName, productPrice, i, id) {
+  function addItemToCart(imageSrc, productName, productPrice, i) {
     var cartRow = document.createElement('div');
     $(cartRow).addClass('cart-row');
-    cartRow.dataset.itemId = id;
     $(cartRow).attr('id', i);
     var cartItems = $('.cart-content');
     var cartItemNames = $('.cart-item-name');
--- js/order.js
+++ js/order.js
@@ -1,75 +1,8 @@
 $(document).ready(function(){
 
-  //Show message when cart is empty
-  function emptyCart() {
-    if($('.cart-content').children().length == 0) {
-      $('.empty-cart-message').show();
-    } else {
-      $('.empty-cart-message').hide();
-    }
-  }
-
-  //Updates the cart total
-  function updateCartTotal() {
-    var cartRows = $('.cart-content').children('.cart-row');
-    var total = 0;
-    for (var i = 0; i < cartRows.length; i++) {
-      var cartRow = cartRows[i];
-      var price = parseFloat($(cartRow).find('.cart-item-price').text().replace('€', ''));
-      var quantity = parseFloat($(cartRow).find('.cart-item-quantity').val());
-      total = total + (price * quantity);
-    }
-    total = Math.round(total * 100) / 100;
-    $('.cart-total').text('€' + total);
-  }
-
-
-
-  var stripeHandler = StripeCheckout.configure({
-    key: stripePublicKey,
-    locale: 'auto',
-    token: function(token) {
-      var items = []
-      var cartRows = $('.cart-content').children('.cart-row');
-      for (var i = 0; i < cartRows.length; i++) {
-        var cartRow = cartRows[i]
-        var quantity = $(cartRow).children('.cart-item-quantity').val()
-        var id = $(cartRow).attr('data-item-id');
-        items.push({
-          id: id,
-          quantity: quantity
-        })
-      }
-
-      fetch('/purchase', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify({
-                stripeTokenId: token.id,
-                items: items
-            })
-        }).then(function(res) {
-            return res.json()
-        }).then(function(data) {
-            alert(data.message)
-            $('.cart-content').children().remove();
-            updateCartTotal();
-            emptyCart();
-            localStorage.clear();
-        }).catch(function(error) {
-            console.error(error)
-        })
-    }
-  })
-
   $('.purchase-button').click(function(){
-    var price = parseFloat($('.cart-total').text().replace('€', '')) * 100
-    stripeHandler.open({
-      amount: price
-    })
+    $('.cart-content').children().remove();
+    localStorage.clear();
   })
 
 })
